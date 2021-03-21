21d54bb7bed72c6f98d21945f9c8e69b8f906e43
Nastya <hukovanasta@gmail.com>
2021-03-21 17:18:03 +0200

    fixed bags

--- drogdrop.js
+++ drogdrop.js
@@ -9,6 +9,8 @@ const btnSave = document.querySelector('.drog-space-save');
 const btnBrowse =  document.querySelector('.drog-space-download');
 console.log(btnBrowse);
 const input = document.querySelector('#fileElem');
+const progressBar = document.querySelector('.progress__inner-loaded');
+console.log(progressBar);
 // const MyForm = document.que
 console.log(input);
 let file;
@@ -19,25 +21,25 @@ const availableFileTypes = {
   'image/jpeg': true,
 }
 
-btnBrowse.onclick = ()=>{
-  input.click() ;
-}
+// btnBrowse.onclick = ()=>{
+//   input.click() ;
+// }
 
 input.addEventListener('change', function() {
   file = this.files[0];
   toggleDropSpaceClass();
   showFile();
+  progressBar.classList.add('active');
   console.log(file.name);
 });
 
 function toggleDropSpaceClass(){
   dropSpace.classList.toggle('active');
-};
+}
 
 dropSpace.addEventListener('dragover' ,(e) => {
   e.preventDefault();
 });
-
 dropSpace.addEventListener('dragenter' ,(e) => {
   toggleDropSpaceClass();
 });
@@ -54,6 +56,7 @@ dropSpace.addEventListener('drop', (e) => {
   }
   [file] = e.dataTransfer.files;
   showFile();
+  progressBar.classList.add('active');
   // toggleDropSpaceClass();
 });
 
@@ -81,21 +84,38 @@ dropSpace.addEventListener('drop', (e) => {
 //     console.log(response);
 //   });
 //});
+// TODO showFile rename to showFileInfo
 function showFile(){
+  // TODO use const instead of let
   let fileType = file.type;
   let fileName = file.name;
   let fileSize = file.size;
-  if (availableFileTypes[fileType]){
-    imgNAme.classList.add('info-title');
-    imgNAme.textContent =  fileName;
-    imgSize.classList.add('info-text');
-    imgSize.textContent = Math.floor(fileSize /1000) + 'kb';
-  }
-  else{
-    alert(' You can drop only file with type: png,jpg,jpeg!');
-  }
+
   toggleDropSpaceClass();
-};
+
+  if (availableFileTypes[fileType] === undefined) {
+    alert(' You can drop only file with type: png,jpg,jpeg!');
+    return
+  }
+
+  imgNAme.classList.add('info-title');
+  imgNAme.textContent = fileName;
+  imgSize.classList.add('info-text');
+  imgSize.textContent = `${Math.floor(fileSize / 1000)}kb`;
+
+  // =================================
+
+  // if (availableFileTypes[fileType]) {
+  //   imgNAme.classList.add('info-title');
+  //   imgNAme.textContent = fileName;
+  //   imgSize.classList.add('info-text');
+  //   imgSize.textContent = `${Math.floor(fileSize / 1000)}kb`;
+  // } else {
+  //   alert(' You can drop only file with type: png,jpg,jpeg!');
+  // }
+  //
+  // toggleDropSpaceClass();
+}
 // function handleFiles(files) {
 //   ([...files]).forEach(uploadFile);
 // }
@@ -121,29 +141,37 @@ formUpload.addEventListener('submit', uploadFile);
 function uploadFile(e){
   e.preventDefault();
   // let file = this.files;
+  // TODO move to separate function uploadFileToServer use Promise
+  /**
+   * await uploadFileToServer()
+   * const urlList = await getAllFilesURLs()
+   * renderImages(urlList)
+   * */
   let url = 'http://localhost:2121/upload';
-  let  xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   let formData = new FormData();
   xhr.open('POST', url, true)
   xhr.addEventListener('load', () => {
-    const response = JSON.parse(xhr.responseText);
-    cb(response);
+    // const response = JSON.parse(xhr.responseText);
+    console.log(xhr.responseText)
+    // cb(response);
   });
- xhr.upload.addEventListener('progress', e => {
+  xhr.upload.addEventListener('progress', e => {
    console.log(e);
- });
-  xhr.setRequestHeader('Content-Type','multipart/form-data');
-  xhr.addEventListener('error', () => {
-    console.log('error');
   });
-  formData.append('file', file);
-   xhr.send(formData);
-};
+
+  xhr.addEventListener('error', () => {
+    console.error('error');
+  });
+  formData.append('files', file);
+  xhr.send(formData);
+}
+
 // btnSave.addEventListener('click', e => {
 //     const newPost = {
-//       body: formData,
+//       body: new FormData(fileElem[0]),
 //     };
-//     sentServer(newPost, response => {
+//     uploadFile(newPost, response => {
 //       console.log(response);
 //     });
 //   });
